5c06c7106d17f54f60dcd0fb6f4c0b0f2a2021f2
Alexander Vankov <alexander.vankov03@gmail.com>
2026-06-13 16:21:59 +0300

    moved data dir into build

--- include/app.hpp
+++ include/app.hpp
@@ -9,9 +9,9 @@ class App {
     void run();
 
     private:
-    const string usersFile = "../data/users.csv";
-    const string vehiclesFile = "../data/vehicles.csv";
-    const string real_estateFile = "../data/real_estate.csv";
+    const string usersFile = "data/users.csv";
+    const string vehiclesFile = "data/vehicles.csv";
+    const string real_estateFile = "data/real_estate.csv";
 
     Database<User> usersDB;
     Database<Vehicle> vehiclesDB;
